838829f9dc6f304f0388f01f0534f98f26b2369a
Vitaliy Baranov <v.baranov@hh.ru>
2023-07-16 16:09:47 +0300

    hw5-jdbc remove unused author dao methods

--- hw5/src/main/java/ru/vitalib/otus/homework/books/dao/AuthorDaoJdbc.java
+++ hw5/src/main/java/ru/vitalib/otus/homework/books/dao/AuthorDaoJdbc.java
@@ -2,17 +2,10 @@ package ru.vitalib.otus.homework.books.dao;
 
 import lombok.AllArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.vitalib.otus.homework.books.domain.Author;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -26,70 +19,10 @@ public class AuthorDaoJdbc implements AuthorDao {
             return jdbc.queryForObject(
               "select id, name from author where name =:name",
               Map.of("name", authorName),
-              new AuthorRowMapper()
+              (rs, rowNum) -> new Author(rs.getLong("id"), rs.getString("name"))
             );
         } catch (EmptyResultDataAccessException ex) {
             return null;
-        }
-    }
-
-    @Override
-    public long save(Author author) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        MapSqlParameterSource parameterSource = new MapSqlParameterSource(Map.of("name", author.getName()));
-        jdbc.update(
-          "insert into author (name) values(:name)",
-          parameterSource,
-          keyHolder
-        );
-        return (long) keyHolder.getKey();
-    }
-
-    @Override
-    public Author findById(long id) {
-        try {
-            return jdbc.queryForObject(
-              "select id, name from author where id =:id",
-              Map.of("id", id),
-              new AuthorRowMapper()
-            );
-        } catch (EmptyResultDataAccessException exception) {
-            return null;
-        }
-    }
-
-    @Override
-    public void delete(long id) {
-        jdbc.update("delete from author where id = :id", Map.of("id", id));
-
-    }
-
-    @Override
-    public void update(long id, Author author) {
-        jdbc.update(
-          "update author set name = :name where id = :id",
-          Map.of("name", author.getName(), "id", author.getId())
-        );
-    }
-
-    @Override
-    @SuppressWarnings("all")
-    public int count() {
-        return jdbc.queryForObject("select count(1) from author", Map.of(), Integer.class);
-    }
-
-    @Override
-    public List<Author> findAll() {
-        return jdbc.query(
-          "select id, name from author",
-          new AuthorRowMapper()
-        );
-    }
-
-    private static class AuthorRowMapper implements RowMapper<Author> {
-        @Override
-        public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return new Author(rs.getLong("id"), rs.getString("name"));
         }
     }
 }
